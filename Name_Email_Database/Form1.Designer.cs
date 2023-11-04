d317283680cac59c927ae344daf20046a35271d0
Gavin Smithson <123982599+Gavin-Smithson@users.noreply.github.com>
2023-11-03 21:53:32 -0400

    Add project files.

---
+++ Name_Email_Database/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Name_Email_CRUD
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLbl = new System.Windows.Forms.Label();
+            this.LookupDeleteGroupBox = new System.Windows.Forms.GroupBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.lookupButton = new System.Windows.Forms.Button();
+            this.lookupDeleteNameLbl = new System.Windows.Forms.Label();
+            this.lookupDeleteNameTextBox = new System.Windows.Forms.TextBox();
+            this.createUpdateGroupBox = new System.Windows.Forms.GroupBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.createButton = new System.Windows.Forms.Button();
+            this.createUpdateEmailLbl = new System.Windows.Forms.Label();
+            this.createUpdateNameLbl = new System.Windows.Forms.Label();
+            this.createUpdateEmailTextBox = new System.Windows.Forms.TextBox();
+            this.createUpdateNameTextBox = new System.Windows.Forms.TextBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.LookupDeleteGroupBox.SuspendLayout();
+            this.createUpdateGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titleLbl
+            // 
+            this.titleLbl.AutoSize = true;
+            this.titleLbl.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLbl.ForeColor = System.Drawing.SystemColors.Control;
+            this.titleLbl.Location = new System.Drawing.Point(28, 9);
+            this.titleLbl.Name = "titleLbl";
+            this.titleLbl.Size = new System.Drawing.Size(517, 38);
+            this.titleLbl.TabIndex = 0;
+            this.titleLbl.Text = "Make sure to enter names and emails exactly as they are spelt. \r\nCapitlization ma" +
+    "tters";
+            this.titleLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // LookupDeleteGroupBox
+            // 
+            this.LookupDeleteGroupBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.LookupDeleteGroupBox.Controls.Add(this.deleteButton);
+            this.LookupDeleteGroupBox.Controls.Add(this.lookupButton);
+            this.LookupDeleteGroupBox.Controls.Add(this.lookupDeleteNameLbl);
+            this.LookupDeleteGroupBox.Controls.Add(this.lookupDeleteNameTextBox);
+            this.LookupDeleteGroupBox.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LookupDeleteGroupBox.ForeColor = System.Drawing.SystemColors.Control;
+            this.LookupDeleteGroupBox.Location = new System.Drawing.Point(12, 85);
+            this.LookupDeleteGroupBox.Name = "LookupDeleteGroupBox";
+            this.LookupDeleteGroupBox.Size = new System.Drawing.Size(262, 236);
+            this.LookupDeleteGroupBox.TabIndex = 1;
+            this.LookupDeleteGroupBox.TabStop = false;
+            this.LookupDeleteGroupBox.Text = "Lookup and Delete";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.SystemColors.Control;
+            this.deleteButton.FlatAppearance.BorderSize = 0;
+            this.deleteButton.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.deleteButton.Location = new System.Drawing.Point(169, 164);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 32);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // lookupButton
+            // 
+            this.lookupButton.BackColor = System.Drawing.SystemColors.Control;
+            this.lookupButton.FlatAppearance.BorderSize = 0;
+            this.lookupButton.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lookupButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lookupButton.Location = new System.Drawing.Point(20, 164);
+            this.lookupButton.Name = "lookupButton";
+            this.lookupButton.Size = new System.Drawing.Size(75, 32);
+            this.lookupButton.TabIndex = 2;
+            this.lookupButton.Text = "Lookup";
+            this.lookupButton.UseVisualStyleBackColor = false;
+            this.lookupButton.Click += new System.EventHandler(this.lookupButton_Click);
+            // 
+            // lookupDeleteNameLbl
+            // 
+            this.lookupDeleteNameLbl.AutoSize = true;
+            this.lookupDeleteNameLbl.Location = new System.Drawing.Point(6, 32);
+            this.lookupDeleteNameLbl.Name = "lookupDeleteNameLbl";
+            this.lookupDeleteNameLbl.Size = new System.Drawing.Size(81, 15);
+            this.lookupDeleteNameLbl.TabIndex = 1;
+            this.lookupDeleteNameLbl.Text = "Enter a name";
+            // 
+            // lookupDeleteNameTextBox
+            // 
+            this.lookupDeleteNameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lookupDeleteNameTextBox.Location = new System.Drawing.Point(6, 48);
+            this.lookupDeleteNameTextBox.Name = "lookupDeleteNameTextBox";
+            this.lookupDeleteNameTextBox.Size = new System.Drawing.Size(250, 23);
+            this.lookupDeleteNameTextBox.TabIndex = 0;
+            // 
+            // createUpdateGroupBox
+            // 
+            this.createUpdateGroupBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.createUpdateGroupBox.Controls.Add(this.updateButton);
+            this.createUpdateGroupBox.Controls.Add(this.createButton);
+            this.createUpdateGroupBox.Controls.Add(this.createUpdateEmailLbl);
+            this.createUpdateGroupBox.Controls.Add(this.createUpdateNameLbl);
+            this.createUpdateGroupBox.Controls.Add(this.createUpdateEmailTextBox);
+            this.createUpdateGroupBox.Controls.Add(this.createUpdateNameTextBox);
+            this.createUpdateGroupBox.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createUpdateGroupBox.ForeColor = System.Drawing.SystemColors.Control;
+            this.createUpdateGroupBox.Location = new System.Drawing.Point(298, 85);
+            this.createUpdateGroupBox.Name = "createUpdateGroupBox";
+            this.createUpdateGroupBox.Size = new System.Drawing.Size(262, 236);
+            this.createUpdateGroupBox.TabIndex = 1;
+            this.createUpdateGroupBox.TabStop = false;
+            this.createUpdateGroupBox.Text = "Create and Update";
+            // 
+            // updateButton
+            // 
+            this.updateButton.BackColor = System.Drawing.SystemColors.Control;
+            this.updateButton.FlatAppearance.BorderSize = 0;
+            this.updateButton.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.updateButton.Location = new System.Drawing.Point(172, 164);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 32);
+            this.updateButton.TabIndex = 2;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = false;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // createButton
+            // 
+            this.createButton.BackColor = System.Drawing.SystemColors.Control;
+            this.createButton.FlatAppearance.BorderSize = 0;
+            this.createButton.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.createButton.Location = new System.Drawing.Point(18, 164);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(75, 32);
+            this.createButton.TabIndex = 2;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = false;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // createUpdateEmailLbl
+            // 
+            this.createUpdateEmailLbl.AutoSize = true;
+            this.createUpdateEmailLbl.Location = new System.Drawing.Point(6, 84);
+            this.createUpdateEmailLbl.Name = "createUpdateEmailLbl";
+            this.createUpdateEmailLbl.Size = new System.Drawing.Size(87, 15);
+            this.createUpdateEmailLbl.TabIndex = 1;
+            this.createUpdateEmailLbl.Text = "Enter an email";
+            // 
+            // createUpdateNameLbl
+            // 
+            this.createUpdateNameLbl.AutoSize = true;
+            this.createUpdateNameLbl.Location = new System.Drawing.Point(6, 32);
+            this.createUpdateNameLbl.Name = "createUpdateNameLbl";
+            this.createUpdateNameLbl.Size = new System.Drawing.Size(81, 15);
+            this.createUpdateNameLbl.TabIndex = 1;
+            this.createUpdateNameLbl.Text = "Enter a name";
+            // 
+            // createUpdateEmailTextBox
+            // 
+            this.createUpdateEmailTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.createUpdateEmailTextBox.Location = new System.Drawing.Point(6, 100);
+            this.createUpdateEmailTextBox.Name = "createUpdateEmailTextBox";
+            this.createUpdateEmailTextBox.Size = new System.Drawing.Size(250, 23);
+            this.createUpdateEmailTextBox.TabIndex = 0;
+            // 
+            // createUpdateNameTextBox
+            // 
+            this.createUpdateNameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.createUpdateNameTextBox.Location = new System.Drawing.Point(6, 48);
+            this.createUpdateNameTextBox.Name = "createUpdateNameTextBox";
+            this.createUpdateNameTextBox.Size = new System.Drawing.Size(250, 23);
+            this.createUpdateNameTextBox.TabIndex = 0;
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.SystemColors.Control;
+            this.exitButton.FlatAppearance.BorderSize = 0;
+            this.exitButton.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.exitButton.Location = new System.Drawing.Point(181, 351);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(204, 32);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Save and exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.ClientSize = new System.Drawing.Size(572, 424);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.createUpdateGroupBox);
+            this.Controls.Add(this.LookupDeleteGroupBox);
+            this.Controls.Add(this.titleLbl);
+            this.Name = "mainForm";
+            this.Text = "Name & Email Database";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.LookupDeleteGroupBox.ResumeLayout(false);
+            this.LookupDeleteGroupBox.PerformLayout();
+            this.createUpdateGroupBox.ResumeLayout(false);
+            this.createUpdateGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLbl;
+        private System.Windows.Forms.GroupBox LookupDeleteGroupBox;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button lookupButton;
+        private System.Windows.Forms.Label lookupDeleteNameLbl;
+        private System.Windows.Forms.TextBox lookupDeleteNameTextBox;
+        private System.Windows.Forms.GroupBox createUpdateGroupBox;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Label createUpdateNameLbl;
+        private System.Windows.Forms.TextBox createUpdateNameTextBox;
+        private System.Windows.Forms.Label createUpdateEmailLbl;
+        private System.Windows.Forms.TextBox createUpdateEmailTextBox;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Name_Email_Database/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections;
+using System.Net.Http.Headers;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace Name_Email_CRUD
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+        // Declare regular expression var for use in create and update
+        private string emailPattern = @"^[\w\.-]+@[\w\.-]+\.\w+$";
+        // Create a dictonary
+        private Dictionary<string, string> nameEmailDictionary = new Dictionary<string, string>();
+        // Declare filepath
+        private string filepath = "data.txt";
+
+
+        private void LoadDictionaryFromTextFile()
+        {
+            // This is a function for loading in the txt file into a dictonary 
+            // Read the CSV data from the text file and update the nameEmailDictionary
+            string[] csvLines = File.ReadAllLines(filepath);
+            foreach (var line in csvLines)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length == 2)
+                {
+                    string name = parts[0];
+                    string email = parts[1];
+                    nameEmailDictionary[name] = email;
+                }
+            }
+        }
+        static void SaveDictionaryToTextFile(string filePath, Dictionary<string, string> dictionary)
+        {
+            // Create a list to hold the CSV lines
+            List<string> csvLines = new List<string>();
+
+            foreach (var kvp in dictionary)
+            {
+                string line = $"{kvp.Key},{kvp.Value}";
+                csvLines.Add(line);
+            }
+
+            // Write the CSV lines to the text file
+            File.WriteAllLines(filePath, csvLines);
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            // Check if the file exists and if it does load it
+            if (File.Exists(filepath))
+            {
+                LoadDictionaryFromTextFile();
+            }
+            else
+            {
+                // Send an error message and tell the user we're using an empty dictonary and you will not save
+
+            }
+        }
+
+
+        private void lookupButton_Click(object sender, EventArgs e)
+        {
+            // TODO: Create a way to lookup a name from an email? I'd have to use a different data structure which complicates things greatly. 
+            
+            
+            // Check to see if the user entered anything in the textbox before startying
+            if (lookupDeleteNameTextBox.Text == "" )
+            {
+                // Tell the user whats wrong 
+                MessageBox.Show("Please enter details in the name field.", "Error! Name field is empty!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Jump out of the onclick
+                return;
+            }
+            // Declare string variable for use within the lookup 
+            string key = lookupDeleteNameTextBox.Text;
+
+            // If the key exists within the dictionary 
+            if (nameEmailDictionary.ContainsKey(lookupDeleteNameTextBox.Text))
+            {
+                MessageBox.Show($"The name {key} does exist within the database\nThe email is {nameEmailDictionary[key]}", ("Name does exist!"), MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            // If the key doesn't exist
+            else
+            {
+                MessageBox.Show($"The name {key} doesn't exist within the database", ("Name doesn't exist!"), MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            // If the key exists within the dictionary
+            if (nameEmailDictionary.ContainsKey(lookupDeleteNameTextBox.Text))
+            {
+                // Delcare a key variable
+                string key = lookupDeleteNameTextBox.Text;
+                // Prompt the user for confirmation just incase they accidentally hit the button
+                var confirmation = MessageBox.Show("After deleteing a user you cannot revert this change\nWould you like to continue", "Are you sure?...", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                // If they pressed yes, delete it and tell the user that it was deleted
+                if (confirmation == DialogResult.Yes)
+                {
+                    // Delete the record
+                    nameEmailDictionary.Remove(key);
+
+                    // TODO: Exception handle on delete.
+                    // There really shouldn't be any errors because if a key exists you normally can delete it, but it might be some good measure to put something here for some users who go above and beyond to break this
+
+                    // Tell the user it was good
+                    MessageBox.Show("User sucessfully deleted", "Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                // If not show the user it is aborting the process
+                else
+                {
+                    // Tell the user it didn't work
+                    MessageBox.Show("Aborting", "Deleting process canceled!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            // Declare Key and Value variables if name and email are entered
+            if (createUpdateNameTextBox.Text == "" || createUpdateEmailTextBox.Text == "")
+            {
+                // Tell the user whats wrong 
+                MessageBox.Show("Please enter details in both name and email", "Error! Name or Email is empty!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Jump out of the onclick
+                return;
+            }
+            // Declare key and value strings
+            string key = createUpdateNameTextBox.Text;
+            string value = createUpdateEmailTextBox.Text;
+
+            // Check to see if the email is a valid email. This isn't super strict
+            if (!Regex.IsMatch(value, emailPattern))
+            {
+                // Tell the user it's not valid
+                MessageBox.Show("Please enter a valid email!", "Error! Invalid email!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Jump out of the onclick
+                return;
+            }
+
+            // Check to see if the key is already there, if so tell the user it already exists
+            if (nameEmailDictionary.ContainsKey(key))
+            {
+                // Tell the user the name already exists
+                MessageBox.Show("Please enter a new name.", "Error! User already exists!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Jump out of the onclick
+                return;
+            }
+
+
+
+
+            // Prompt the user for confirmation just incase they accidentally hit the button
+            var confirmation = MessageBox.Show($"You are about the create the user{key},{value}\nWould you like to continue", "Are you sure?...", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            // If they pressed yes, create it and tell the user that it was create
+            if (confirmation == DialogResult.Yes)
+            {
+                // Create the record
+                nameEmailDictionary[key] = value;
+
+                // TODO: Exception handle on create.
+                // Now there REALLY REALLY shouldn't be errors on this but endusers might break something
+
+                // Tell the user it was good
+                MessageBox.Show("User sucessfully created", "Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            // If not show the user it is aborting the process
+            else
+            {
+                // Tell the user it was aborted
+                MessageBox.Show("Aborting", "Creation process canceled!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            // Declare Key and Value variables if name and email are entered
+            if (createUpdateNameTextBox.Text == "" || createUpdateEmailTextBox.Text == "")
+            {
+                // Tell the user whats wrong 
+                MessageBox.Show("Please enter details in both name and email", "Error! Name or Email is empty!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Jump out of the onclick
+                return;
+            }
+            // Declare key and value strings
+            string key = createUpdateNameTextBox.Text;
+            string updatedValue = createUpdateEmailTextBox.Text;
+            string oldValue = nameEmailDictionary[key];
+
+            // Check to see if the email is a valid email. This isn't super strict
+            if (!Regex.IsMatch(updatedValue, emailPattern))
+            {
+                // Tell the user it's not valid
+                MessageBox.Show("Please enter a valid email!", "Error! Invalid email!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Jump out of the onclick
+                return;
+            }
+
+            // Prompt the user for confirmation just incase they accidentally hit the button
+            var confirmation = MessageBox.Show($"You are about to change the email from {oldValue} to {updatedValue} ,Would you like to continue", "Are you sure?...", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            // If they pressed yes, create it and tell the user that it was create
+            if (confirmation == DialogResult.Yes)
+            {
+                // Create the record
+                nameEmailDictionary[key] = updatedValue;
+
+                // TODO: Exception handle on update.
+                // Possible errors? I'm not too sure since's youre just updating a value, but if i can break it will break
+
+                // Tell the user it was good
+                MessageBox.Show("User sucessfully updated", "Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            // If not show the user it is aborting the process
+            else
+            {
+                // Tell the user it was aborted
+                MessageBox.Show("Aborting", "Update process canceled!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Call the save function
+            SaveDictionaryToTextFile(filepath, nameEmailDictionary);
+            // Tell the user its all good
+            MessageBox.Show("Saved sucessfully!","Sucess!",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            // Close
+            this.Close();
+        }
+    }
+}
